eea517616efa8d85fef816570cd88c07a0c1b41f
zechaos031 <delrieu.adrien@gmail.com>
2021-06-17 01:33:04 +0200

    :wrench: Handle level (not implemented)

--- src/Events/bot/message.js
+++ src/Events/bot/message.js
@@ -1,11 +1,11 @@
 module.exports = async (client,message) =>{
     const Automod = new(require(`../../Utils/Service/Automod`))(client)
+    const Levels = new(require(`../../Utils/Service/Levels`))(client)
 
     if (message.author.bot) return;
     if (!message.guild) return;
     let GuildData = await client.db.getData('Guild',{id:message.guild.id})
 
-    console.info(GuildData)
     await Automod.handle(message)
 
 
