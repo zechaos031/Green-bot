1dc340f1a9f413062a86a92707b7f7968ad56d68
zechaos <delrieu.adrien@gmail.com>
2021-06-21 09:13:00 +0200

    🔧 Fix create counter

--- src/Utils/Service/Counter.js
+++ src/Utils/Service/Counter.js
@@ -1,58 +1,64 @@
-class Counter{
+class Counter {
     constructor(client) {
         this.client = client
     }
 
-    async createCounter(channel, { type,data }){
-        let dataGuild = await this.client.db.getData("Guild",{id:channel.guild.id})
+    async createCounter(guild) {
+        let dataGuild = await this.client.db.getData("Guild", { id: guild.id })
         /**
          * TODO
          *  - Continue counter
          */
 
-        console.log(type,data)
-        switch ( type ){
-            case 'member':
-                dataGuild.counter.channel.channelMemberID = await Counter.createChannel(channel.guild,`Membre en cache: ${ channel.guild.members.cache.filter(m => !m.user.bot).map(g=>g).length }`)
-                await this.client.db.updateData('Guild', { id: channel.guild.id }, { counter: dataGuild.counter })
-
-                break
-            case "bot":
-                dataGuild.counter.channel.channelBotID = await Counter.createChannel(channel.guild,`Bot: ${ channel.guild.members.cache.filter(m => m.user.bot).map(g=>g).length }`)
-                await this.client.db.updateData('Guild', { id: channel.guild.id }, { counter: dataGuild.counter })
-
-
-                break
-            case "total":
-                dataGuild.counter.channel.ChannelTotalID = await Counter.createChannel(channel.guild,`Membre Total: ${ channel.guild.memberCount }`)
-                await this.client.db.updateData('Guild', { id: channel.guild.id }, { counter: dataGuild.counter })
-
-
-                break
-            default:
-
-                break
-        }
-
+        await this.createChannel(guild,dataGuild)
 
     }
 
 
-     static async createChannel(guild,data){
-        let categorie =  guild.channels.cache.filter(c =>c.type ==='category').filter(cat =>cat.name ==="Counter").first()
-        if (!categorie){
-            categorie = await guild.channels.create('Counter',{
-                type:'category',
+    async createChannel(guild,data) {
+        let categorie = guild.channels.cache.filter(c => c.type === 'category').filter(cat => cat.name === "Counter").first()
+        if (!categorie) {
+            categorie = await guild.channels.create('Counter', {
+                type: 'category',
             })
             await categorie.setPosition(0)
         }
-          let channel = await guild.channels.create(data, {
-             type: "voice",
-             parent: categorie.id,
-             deny: ['CONNECT']
-         })
-         return channel
+        let channelMember = guild.channels.cache.get(data.counter.channel.channelMemberID)
 
+        if(!channelMember){
+             guild.channels.create(`Membre en cache: ${guild.members.cache.filter(m => !m.user.bot).map(g => g).length}`, {
+                 type: "voice",
+                 parent: categorie.id,
+                 deny: ['CONNECT']
+             }).then( async r =>{
+                 data.counter.channel.channelMemberID = r.id
+             })
+        }
+        let channelBot = guild.channels.cache.get(data.counter.channel.channelBotID)
+
+        if(!channelBot){
+            guild.channels.create(`Bot: ${guild.members.cache.filter(m => m.user.bot).map(g => g).length}`, {
+                type: "voice",
+                parent: categorie.id,
+                deny: ['CONNECT']
+            }).then( async r =>{
+                data.counter.channel.channelBotID = r.id
+            })
+        }
+
+        let channelTotal = guild.channels.cache.get(data.counter.channel.ChannelTotalID)
+
+        if(!channelTotal){
+            guild.channels.create(`Membre Total: ${guild.memberCount}`, {
+                type: "voice",
+                parent: categorie.id,
+                deny: ['CONNECT']
+            }).then( async r =>{
+                data.counter.channel.ChannelTotalID = r.id
+
+            })
+        }
+        await this.client.db.updateData('Guild', { id: guild.id }, { counter: data.counter })
 
     }
 }
